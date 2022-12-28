0b575ca115693da796b6980825b820c84804bc47
Ana Bujan <ana@eisberg-labs.com>
2022-12-28 08:15:22 +0100

    refactor: reformat derive to use quote

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,27 @@
 extern crate proc_macro;
+extern crate quote;
 
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 /// Reduces boilerplate for actix view models
 /// Note: type Error has to be in context ( define enum Error or import Error)
 #[proc_macro_derive(JsonResponder)]
 pub fn responder_derive(input: TokenStream) -> TokenStream {
-    let ast = parse_macro_input!(input as DeriveInput);
-    let struct_name = &ast.ident.to_string();
-    let generic_impl = "
-    impl actix_web::Responder for {} {
-        type Body = actix_web::body::BoxBody;
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = input.ident.clone();
 
-        fn respond_to(self, _: &actix_web::HttpRequest) -> actix_web::HttpResponse<Self::Body> {
-            match serde_json::to_string(&self) {
-                Err(err) => actix_web::HttpResponse::from_error(err),
-                Ok(value) => actix_web::HttpResponse::Ok().content_type(\"application/json\").body(value)
+    (quote! {
+        impl actix_web::Responder for #ident {
+            type Body = actix_web::body::BoxBody;
+
+            fn respond_to(self, _: &actix_web::HttpRequest) -> actix_web::HttpResponse<Self::Body> {
+                match serde_json::to_string(&self) {
+                    Err(err) => actix_web::HttpResponse::from_error(err),
+                    Ok(value) => actix_web::HttpResponse::Ok().content_type("application/json").body(value)
+                }
             }
         }
-    }
-    ";
-    generic_impl
-        .replace("{}", struct_name.as_str())
-        .parse()
-        .unwrap()
+    }).into()
 }
